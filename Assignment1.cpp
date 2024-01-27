58b706694e883ab04be1c07873a3253b7dcbc794
Ethan Releford <90151318+ethanreleford@users.noreply.github.com>
2024-01-26 23:51:08 -0500

    Third Commit

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -72,7 +72,7 @@ void countPrimesInRange(int start, int end)
     sumOfPrimes += localSum;
 
     //merge top ten primes into the global top ten primes
-    for (int i = 0; i < topTenSize; ++i)
+    for (int i = topTenSize - 1; i >= 0; --i)
     {
         if (localTopTenPrimes[i] == 0)
         {
@@ -127,7 +127,7 @@ int main()
                    << primeCount << " " << sumOfPrimes << std::endl;
 
         //write top ten primes to file
-        for (int i = 0; i < topTenSize; ++i)
+        for (int i = topTenSize - 1; i >= 0; --i)
         {
             if (topTenPrimes[i] != 0)
             {
